72e3fd38772fccb8cfb2c38751745f96e3f3c55c
abdul99j <abdul.dps@gmail.com>
2021-01-01 18:40:25 +0500

    Final

---
+++ views/validateApplication.js
@@ -0,0 +1,6 @@
+var firstName=document.getElementsByName('firstName');
+var lasttName=document.getElementsByName('lastName');
+var email=document.getElementsByName('email');
+var experience=document.getElementsByName('experience');
+var qualification=document.getElementsByName('qualification');
+var nic=document.getElementsByName('nic');
