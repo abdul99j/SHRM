ea54cd5e294880b730aef76e4df6824f6ddd7a78
abdul99j <abdul.dps@gmail.com>
2021-04-01 22:10:10 +0500

    Login to be integrated

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 const url=require('url');
 const passport = require('passport');
